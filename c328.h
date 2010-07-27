7918bd379015f5fb750369fa2b7347d0141fdd04
Philip Heron <phil@sanslogic.co.uk>
2010-07-27 11:06:14 +0100

    Add GPL text

--- c328.h
+++ c328.h
@@ -1,3 +1,11 @@
+/* hadie - High Altitude Balloon flight software              */
+/*============================================================*/
+/* Copyright (C)2010 Philip Heron <phil@sanslogic.co.uk>      */
+/*                                                            */
+/* This program is distributed under the terms of the GNU     */
+/* General Public License, version 2. You may use, modify,    */
+/* and redistribute it under the terms of this license. A     */
+/* copy should be included with this source.                  */
 
 #ifndef INC_C328_H
 #define INC_C328_H
